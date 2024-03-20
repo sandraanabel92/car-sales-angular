fa6c23dcd17769940cb218f5afd223b33e484a98
Sandra Palacios <sandraanabel21@gmail.com>
2024-03-19 19:43:21 -0500

    Creación de la carpeta utilitarios y cambios en la PagListaVehiculos

--- src/app/paginas/PaginaModule.ts
+++ src/app/paginas/PaginaModule.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { PagListaVehiculosComponent } from "./PagListaVehiculos/PagListaVehiculos.component";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { UtilitariosModule } from "../utilitarios/UtilitariosModule";
 
 @NgModule({
   imports:[
     CommonModule,
-    FormsModule
+    FormsModule,
+    UtilitariosModule
   ],
   declarations:[
     PagListaVehiculosComponent
---
+++ src/app/utilitarios/UtilitariosModule.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import {AEspacioPipe} from "./pipes/AEspacio.pipe";
+
+@NgModule({
+  declarations:[
+    AEspacioPipe
+  ],
+  imports:[],
+  exports:[
+    AEspacioPipe
+  ]
+
+
+})
+
+export class UtilitariosModule{
+
+}
