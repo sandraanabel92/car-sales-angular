8de39e57a97862b837d0e722112bd96f6e99682f
Sandra Palacios <sandraanabel21@gmail.com>
2024-03-20 22:37:14 -0500

    Creación de la PagNotFound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './paginas/home/home.component';
 import { PagListaVehiculosComponent } from './paginas/PagListaVehiculos/PagListaVehiculos.component';
+import { PageNotFoundComponent } from './paginas/home/PagNotFound/PageNotFound.component';
 
 const routes: Routes = [
   {
@@ -20,7 +21,7 @@ const routes: Routes = [
   },
   {
     path:"**",
-    component: HomeComponent,
+    component: PageNotFoundComponent,
     pathMatch: "full"
   }
 ];
