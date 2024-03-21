d41998a78de5b21666fd8a464a2568460dd09512
Sandra Palacios <sandraanabel21@gmail.com>
2024-03-20 21:32:47 -0500

    Creacion de la pagina Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './paginas/home/home.component';
+import { PagListaVehiculosComponent } from './paginas/PagListaVehiculos/PagListaVehiculos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path:"vehiculos",
+    component:PagListaVehiculosComponent
+  },
+  {
+    path: "",
+    component: HomeComponent,
+    pathMatch: "full"
+
+  },
+  {
+    path:"**",
+    component: HomeComponent,
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
